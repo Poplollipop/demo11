9ebcd4cf1d623c93185e9d4440ac2ad0c10ce8dc
李典学 <>
2024-10-24 09:38:09 +0800

    add InterfaceTest

--- src/test/java/com/example/demo11/Demo11ApplicationTests.java
+++ src/test/java/com/example/demo11/Demo11ApplicationTests.java
@@ -1,27 +1,14 @@
 package com.example.demo11;
 
-import org.junit.jupiter.api.Test;
+
 import org.springframework.boot.test.context.SpringBootTest;
 
-<<<<<<< HEAD
+
 @SpringBootTest // 先從程式入口點開始運作，會先完成程式的基本配置、再執行測試內容
-=======
-@SpringBootTest
->>>>>>> c1bf31048463ad85b4d7f284bd4bd07c8be58a75
+
+
+
 class Demo11ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
-<<<<<<< HEAD
-	@Test
-	public void interfaceTest(){
-		MyInterface my = new MyInterfaceImpl();
-		System.out.println(my.NUM);
-		System.out.println(MyInterface.NUM2);
-	}
-
-=======
->>>>>>> c1bf31048463ad85b4d7f284bd4bd07c8be58a75
 }
--- src/test/java/com/example/demo11/Id.java
+++ src/test/java/com/example/demo11/Id.java
@@ -1,10 +1,12 @@
+package com.example.demo11;
+
 import java.util.Scanner;
 
 public class Id {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String pattern = "[A-Za-z&&[^ABDEFHabdefh]][1-2]\\d{8}";
-        System.out.println("請輸入身分證字號，共10碼： ");
+        System.out.println("請輸入身分證字號,共10碼: ");
         String input = scan.next();
         System.out.println(input.matches(pattern));
     }
